c5cfb1553ef083bb0352c163afb15397751c7662
Yuke_Lenovo <86759162@qq.com>
2016-07-10 21:28:59 +0800

    在本地的git修改了

--- yuke/src/main/java/Maven_Demo/yuke/helloyuke.java
+++ yuke/src/main/java/Maven_Demo/yuke/helloyuke.java
@@ -4,5 +4,6 @@ public class helloyuke{
 	public static void main(String[] args) {
 		System.out.println("hello yuke");
 		System.out.println("在远程的git仓库修改了");
+		System.out.println("在本地的git修改了");
 	}
 }
